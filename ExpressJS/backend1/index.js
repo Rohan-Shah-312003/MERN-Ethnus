e5e006405164db8b1632c440e8ff262a4a6ebbaf
Rohan_Shah <rohan312003@gmail.com>
2024-07-01 12:44:00 +0530

    01-07-2024

--- ExpressJS/backend1/index.js
+++ ExpressJS/backend1/index.js
@@ -1,1 +1,10 @@
-const express = require('express');
+const express = require("express");
+const router = require("./controller/router");
+const app = express();
+
+
+app.use("/routes", router);
+
+app.listen(5000, () => {
+  console.log("Application is running in the port 5000");
+});
