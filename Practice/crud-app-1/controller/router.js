66a3e102e7bbbd9291098dff66b95e612184bb6e
Rohan_Shah <rohan312003@gmail.com>
2024-07-03 00:25:55 +0530

    02-07-2024

--- Practice/crud-app-1/controller/router.js
+++ Practice/crud-app-1/controller/router.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/usersSchema");
+const multer = require("multer");
+
+// image upload
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldName + "_" + Date.now() + "_" + file.originalname);
+  },
+});
+
+// middleware
+var upload = multer({
+  storage: storage,
+}).single("image");
 
 router.get("/", (req, res) => {
-  res.send("Home Page");
+  res.render("index", {
+    title: "Home Page",
+  });
+});
+
+router.get("/add", (req, res) => {
+  res.render("add_users", { title: "Add Users" });
 });
 
 module.exports = router;
