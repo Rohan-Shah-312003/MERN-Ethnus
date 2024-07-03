1ee9916949801980f05a63c106e4bf282aca911c
Rohan_Shah <rohan312003@gmail.com>
2024-07-03 11:34:49 +0530

    03-07-2024

--- Practice/crud-app-1/controller/router.js
+++ Practice/crud-app-1/controller/router.js
@@ -18,6 +18,42 @@ var upload = multer({
   storage: storage,
 }).single("image");
 
+// Insert a user to DB
+router.post("/add", upload, (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    image: req.file.filename,
+  });
+  user
+    .save()
+    .then(() => {
+      req.session.message = {
+        type: "success",
+        message: "User added successfully",
+      };
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.json({ message: err.message, type: "danger" });
+    });
+});
+
+// Get all users
+router.get("/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.render("index", {
+        title: "Home Page",
+        users: users,
+      });
+    })
+    .catch((err) => {
+      res.json({ message: err.message, type: "danger" });
+    });
+});
+
 router.get("/", (req, res) => {
   res.render("index", {
     title: "Home Page",
